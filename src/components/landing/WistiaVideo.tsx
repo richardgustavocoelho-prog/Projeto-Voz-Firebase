e7dbae8d043207d4aad15595e77f548f105a7d08
Richard Gustavo Sardinha Coelho <richardgustavocoelho@gmail.com>
2025-10-07 23:13:27 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- src/components/landing/WistiaVideo.tsx
+++ src/components/landing/WistiaVideo.tsx
@@ -42,12 +42,14 @@ export function WistiaVideo() {
         `}</style>
         <div className="wistia_responsive_padding">
           <div className="wistia_responsive_wrapper">
-            <div 
-              className="wistia_embed wistia_async_5f16lbgntl videoFoam=true" 
+            <div
+              className="wistia_embed wistia_async_5f16lbgntl videoFoam=true"
               style={{height:"100%", position:"relative", width:"100%"}}
-              dangerouslySetInnerHTML={{ __html: `<script src="https://fast.wistia.com/embed/medias/5f16lbgntl.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>` }}
+              dangerouslySetInnerHTML={{ __html: `&nbsp;` }}
             >
             </div>
+            <Script src="https://fast.wistia.com/embed/medias/5f16lbgntl.jsonp" strategy="lazyOnload" />
+            <Script src="https://fast.wistia.com/assets/external/E-v1.js" strategy="lazyOnload" />
           </div>
         </div>
     </div>

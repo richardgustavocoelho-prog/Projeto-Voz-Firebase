ae55c085de969112dc440c74beba0f854804b26f
Richard Gustavo Sardinha Coelho <richardgustavocoelho@gmail.com>
2025-10-07 22:58:33 +0000

    Arrume esses dois erros

--- src/components/landing/WistiaVideo.tsx
+++ src/components/landing/WistiaVideo.tsx
@@ -4,17 +4,51 @@ import Script from 'next/script';
 
 export function WistiaVideo() {
   return (
-    <div className="vsl-container">
+    <div className="vsl-container bg-black">
       <Script src="https://fast.wistia.com/player.js" strategy="lazyOnload" />
       <style>{`
-          wistia-player[media-id='5f16lbgntl']:not(:defined) { 
-            background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/5f16lbgntl/swatch'); 
-            display: block; 
-            filter: blur(5px); 
-            padding-top:56.25%; 
+          .wistia_responsive_padding {
+            padding: 56.25% 0 0 0;
+            position: relative;
+          }
+          .wistia_responsive_wrapper {
+            height: 100%;
+            left: 0;
+            position: absolute;
+            top: 0;
+            width: 100%;
+          }
+          .wistia_embed {
+            height: 100%;
+            position: relative;
+            width: 100%;
+          }
+          .wistia_swatch {
+            height:100%;
+            left:0;
+            opacity:0;
+            overflow:hidden;
+            position:absolute;
+            top:0;
+            transition:opacity 200ms;
+            width:100%;
+          }
+          .wistia_swatch > img {
+            filter:blur(5px);
+            height:100%;
+            object-fit:contain;
+            width:100%;
           }
         `}</style>
-        <wistia-player media-id="5f16lbgntl" aspect="1.7777777777777777"></wistia-player>
+        <div className="wistia_responsive_padding">
+          <div className="wistia_responsive_wrapper">
+            <div className="wistia_embed wistia_async_5f16lbgntl videoFoam=true" style={{height:"100%", position:"relative", width:"100%"}}>
+              <div className="wistia_swatch" style={{height:"100%",left:0,opacity:0,overflow:"hidden",position:"absolute",top:0,transition:"opacity 200ms",width:"100%"}}>
+                <img src="https://fast.wistia.com/embed/medias/5f16lbgntl/swatch" style={{filter: "blur(5px)",height:"100%",objectFit:"contain",width:"100%"}} alt="" aria-hidden="true" />
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   );
 }

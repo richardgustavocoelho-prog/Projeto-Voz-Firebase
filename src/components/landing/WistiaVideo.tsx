600eb76364f294f2a74fcccdee546526b2692a31
Richard Gustavo Sardinha Coelho <richardgustavocoelho@gmail.com>
2025-10-07 22:38:05 +0000

    Try fixing this error: `Console Error: Error: Failed to fetch. Error sou

--- src/components/landing/WistiaVideo.tsx
+++ src/components/landing/WistiaVideo.tsx
@@ -1,29 +1,12 @@
 "use client";
 
-import { useEffect, useRef } from 'react';
+import Script from 'next/script';
 
 export function WistiaVideo() {
-  const videoContainerRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (videoContainerRef.current && !videoContainerRef.current.querySelector('script')) {
-      const playerScript = document.createElement('script');
-      playerScript.src = 'https://fast.wistia.com/player.js';
-      playerScript.async = true;
-
-      const embedScript = document.createElement('script');
-      embedScript.src = 'https://fast.wistia.com/embed/5f16lbgntl.js';
-      embedScript.async = true;
-      embedScript.type = 'module';
-      
-      videoContainerRef.current.appendChild(playerScript);
-      videoContainerRef.current.appendChild(embedScript);
-    }
-  }, []);
-
   return (
-    <div ref={videoContainerRef} className="vsl-container">
-       <style>{`
+    <div className="vsl-container">
+      <Script src="https://fast.wistia.com/player.js" strategy="lazyOnload" />
+      <style>{`
           wistia-player[media-id='5f16lbgntl']:not(:defined) { 
             background: center / contain no-repeat url('https://fast.wistia.com/embed/medias/5f16lbgntl/swatch'); 
             display: block; 

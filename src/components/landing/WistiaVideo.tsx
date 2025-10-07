328353128b091600211084b434b58c3c92b28227
Richard Gustavo Sardinha Coelho <richardgustavocoelho@gmail.com>
2025-10-07 23:12:09 +0000

    Adicione esse video aqui: <script src="https://fast.wistia.com/player.js

--- src/components/landing/WistiaVideo.tsx
+++ src/components/landing/WistiaVideo.tsx
@@ -42,10 +42,11 @@ export function WistiaVideo() {
         `}</style>
         <div className="wistia_responsive_padding">
           <div className="wistia_responsive_wrapper">
-            <div className="wistia_embed wistia_async_5f16lbgntl videoFoam=true" style={{height:"100%", position:"relative", width:"100%"}}>
-              <div className="wistia_swatch" style={{height:"100%",left:0,opacity:0,overflow:"hidden",position:"absolute",top:0,transition:"opacity 200ms",width:"100%"}}>
-                <img src="https://fast.wistia.com/embed/medias/5f16lbgntl/swatch" style={{filter: "blur(5px)",height:"100%",objectFit:"contain",width:"100%"}} alt="" aria-hidden="true" />
-              </div>
+            <div 
+              className="wistia_embed wistia_async_5f16lbgntl videoFoam=true" 
+              style={{height:"100%", position:"relative", width:"100%"}}
+              dangerouslySetInnerHTML={{ __html: `<script src="https://fast.wistia.com/embed/medias/5f16lbgntl.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script>` }}
+            >
             </div>
           </div>
         </div>

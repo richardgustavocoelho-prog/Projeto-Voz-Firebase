6af909e9fa9ada83ff6c6ab9208f614632288993
Richard Gustavo Sardinha Coelho <richardgustavocoelho@gmail.com>
2025-11-12 01:31:44 +0000

    troque essa imagem da foto por essa: https://i.imgur.com/EWXmEIg.png

--- src/components/landing/BonusSection.tsx
+++ src/components/landing/BonusSection.tsx
@@ -22,7 +22,7 @@ export function BonusSection() {
             >
               <div className="relative w-full aspect-[4/3] mb-6">
                 <Image
-                  src="https://i.imgur.com/n1qbgtm.png"
+                  src="https://i.imgur.com/EWXmEIg.png"
                   alt="Ilustração de um contrato em uma prancheta"
                   fill
                   className="object-contain"
